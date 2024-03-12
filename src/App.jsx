d0eb305e4ff765e1135bb4696fc09d10b6c37433
Rayhan Ramadhani <58447376+RayhanRamadhani@users.noreply.github.com>
2024-03-12 03:34:20 +0000

    eoeoeo

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Rayhan Ramadhani",
+  title: "Web Designer, Software Engineer, etc.",
+  email: "gedangdoyan@example.com",
+  gitHub: "RayhanRamadhani",
+  instagram: "rayhann_r",
+  linkedIn: "-",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "-",
+  youTube: "HB Chart",
 };
 
 const primaryColor = "#4E567E";
